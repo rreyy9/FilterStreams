db769ae2de54df5a10d2fed99af005d480dc902c
rreyy9 <94891863+rreyy9@users.noreply.github.com>
2024-11-20 20:53:47 +1100

    corrections

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import DataList from './components/DataList';
 function App() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">My Data App</h1>
       <DataList />
     </div>
   );
